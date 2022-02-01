dd662530e1c95050c051dd384ae7583d2d38f456
Edouard-Germain <edangermain@gmail.com>
2022-02-01 14:41:45 +0100

    dbConnect'

---
+++ dbConfig/db.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+const DB_URL = process.env.DB_URL
+
+const dbConnect = () => {
+  console.log(process.env.DB_URL)
+    const dbName = 'serge'  
+    try {
+      mongoose.connect(DB_URL)
+      console.log(`Connected to ${dbName} database`)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  
+  module.exports = { 
+    dbConnect
+  }
