b2ed4f05ddcbf2d648e802abed1e1b11c32971df
becca <rebecca.kaci@gmail.com>
2022-02-08 15:42:36 +0100

    fix

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT
 const mongoose = require("mongoose")
-<<<<<<< HEAD
-=======
 const morgan = require("morgan")
-const cors = require("cors")
 
 console.log("origin",process.env.ALLOWED_DOMAIN);
 
->>>>>>> 41e47ebfd63a9308c12667d9207c7385abc52d72
 const passport = require("./config/passport")
 const session = require("express-session")
-const morgan = require("morgan")
 const cors = require("cors")
 
 const { dbConnect } = require ('./config/db')
@@ -36,12 +31,6 @@ app.use(cors({
     origin: process.env.ALLOWED_DOMAIN,
     credentials: true
 }))
-
-<<<<<<< HEAD
-=======
-app.use(express.json())
-app.use(morgan("tiny"))
->>>>>>> 41e47ebfd63a9308c12667d9207c7385abc52d72
 
 app.use(session({
     secret: "secret",
